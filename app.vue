0bc8a68c93f781a70b65015de01ab3f001307aa0
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-03-09 10:50:41 -0800

    building the car card

--- app.vue
+++ app.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <!-- NAVBAR -->
-    <header class="
+  <header class="
       flex
       sticky
       top-0
@@ -16,33 +15,9 @@
       <a href="/" class="
       text-3xl font-mono">cartrader</a>
     </header>
-    <!-- NAVBAR -->
-    <!-- MAIN -->
-    <div 
-    class="main
-    h-screen
-    relative
-    w-100
-    bg-no-repeat
-    bg-cover
-    bg-bottom">
-    <!-- MAIN -->
-  <div class="z-10 absolute w-full h-full flex justify-center items-center">
-    <div class="text-center w-1/2">
-      <h1 class="text-white text-8xl font-serif mb-10">Find your car</h1>
-      <div class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
-        <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="Search by city...">
-        <button class="bg-sky-500 px-10 text-white">Search</button>
-      </div>
-    </div>
-  </div>
-  <div class="w-full h-full bg-black bg-opacity-50 z-1 absolute"></div>
-</div>
+
+    <!-- CARS PAGE -->
+    <!-- CARS PAGE -->
 </div>
 </template>
 
-<style>
-.main {
-  background-image: url(./assets/images/car.jpg);
-} 
-</style>
