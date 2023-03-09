89788fdcf0e7580c5e612f4cd78e1573a3601322
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-03-09 15:39:36 -0800

    addidng programatic navigation

--- components/CarSearch.vue
+++ components/CarSearch.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
-        <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="Search by city...">
-        <button class="bg-sky-500 px-10 text-white">Search</button>
+        <input :class="SearchError ? 'border-red-500 border' : ''" v-model="city" type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="Search by city...">
+        <button @click="handleSearch" class="bg-sky-500 px-10 text-white">Search</button>
     </div>
 </template>
+<script setup>
+const city = ref('');
+const router = useRouter();
+const SearchError = ref(true)
+
+const handleSearch = () => {
+    if(!city.value) {
+        return SearchError = true
+    }
+    navigateTo(`city/${city.value}/car`)
+} 
+
+
+</script>
