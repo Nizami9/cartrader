018f76c7f86d5cb5d4bade6084f10d8e2bef7d5d
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-03-08 16:40:27 -0800

    built the navbar and main section

--- app.vue
+++ app.vue
@@ -1,5 +1,44 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- NAVBAR -->
+    <header class="
+      flex
+      sticky
+      top-0
+      z-50
+      justify-between
+      items-center
+      space-x-1
+      border-b
+      bg-white
+      p-4
+      shadow-md">
+      <a href="/" class="
+      text-3xl font-mono">cartrader</a>
+    </header>
+    <!-- NAVBAR -->
+    <!-- MAIN -->
+    <div 
+    class="main
+    h-screen
+    relative
+    w-100
+    bg-no-repeat
+    bg-cover
+    bg-bottom">
+    <!-- MAIN -->
+  <div class="z-10 absolute w-full h-full flex justify-center items-center">
+    <div class="text-center w-1/2">
+      <h1 class="text-white text-8xl font-serif mb-10">Find your car</h1>
+    </div>
   </div>
+  <div class="w-full h-full bg-black bg-opacity-50 z-1 absolute"></div>
+</div>
+</div>
 </template>
+
+<style>
+.main {
+  background-image: url(./assets/images/car.jpg);
+} 
+</style>
