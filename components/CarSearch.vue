23194f76003ff942124daac5ceef097dd65e3296
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-03-09 16:31:46 -0800

    addidnt title to /car/nameOfCar

--- components/CarSearch.vue
+++ components/CarSearch.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
-        <input :class="SearchError ? 'border-red-500 border' : ''" v-model="city" type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="Search by city...">
+        <input :class="SearchError ? 'border-white-500' : ''" v-model="city" type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none" placeholder="Search by city...">
         <button @click="handleSearch" class="bg-sky-500 px-10 text-white">Search</button>
     </div>
 </template>
--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -19,4 +19,13 @@
         </div>
 </div>
 </template>
-<script setup></script>
+<script setup>
+const route = useRoute()
+useHead({
+    title: route.params.name
+})
+// function toTitleCase(str) {
+//     return str.replace(/\w\S*/g, function (txt) {
+//         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+//     })
+</script>
