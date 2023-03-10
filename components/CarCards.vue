e908ef5b3d929f35201a244749ea7ab7e3b003d8
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-03-10 11:46:18 -0800

    adding props

--- components/CarCards.vue
+++ components/CarCards.vue
@@ -1,7 +1,9 @@
 <template>
       <div class="w-full">
-        <CarCard />
+        <CarCard v-for="car in cars" :key="car.id" :car="car" />
       </div>
 </template>
 
-<script setup></script>
+<script setup>
+const {cars} = useCars()
+</script>
