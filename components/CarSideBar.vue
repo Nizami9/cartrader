3e1167d0fe3f7f968cd3d3b9555e00d481ee8ecd
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-03-10 17:30:27 -0800

    updating the location

--- components/CarSideBar.vue
+++ components/CarSideBar.vue
@@ -2,11 +2,11 @@
     <div class="shadow border w-1/2 mr-10 z-30 h-[190px]">
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Location</h3>
-      <h3 class="text-blue-400 capitalize">Toronto</h3>
-      <!-- <div class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white">
-        <input type="text" class="border p-1 rounded">
-        <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
-      </div> -->
+      <h3 @click="updateModal('location')" class="text-blue-400 capitalize">Toronto</h3>
+        <div v-if="modal.location" class="absolute border shadow left-96 p-5 top-1 -m-1 bg-white">
+          <input type="text" class="border p-1 rounded">
+          <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
+        </div>
     </div>
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Make</h3>
@@ -19,3 +19,14 @@
   </div>
 
 </template>
+<script setup>
+const modal = ref({
+  location: false,
+  make: false,
+  price: false
+}) 
+
+const updateModal = (key) => {
+  modal.value[key] = !modal.value[key];
+} 
+</script>
