f68b3c54e82a31efa1e2eaeeaa182f85311b134f
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-03-10 16:18:17 -0800

    made dynamic rendering the carcard

--- components/CarCard.vue
+++ components/CarCard.vue
@@ -1,16 +1,20 @@
 <template>
-     <div class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
+     <div @click="navigateTo(`/car/${car.name}-${car.id}`)" class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
       <div class="flex h-full">
-        <img src="https://www.bentleymotors.com/content/dam/bentley/Master/homepage%20carousel/1920x1080_bentayga_ewb_2.jpg/_jcr_content/renditions/original.image_file.700.394.file/1920x1080_bentayga_ewb_2.jpg"
+        <img :src="car.url"
         class="w-[300px] h-full" alt="car1">
         <div class="p-4 flex flex-col">
           <div>
-            <h1 class="text-2xl text-blue-700">VOLVO XC400</h1>
-            <p class="text-gray-7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, sit. Ipsam temporibus exercitationem quibusdam beatae obcaecati facere sapiente voluptate, debitis, aspernatur nam nemo! Voluptate qui eum ut. Modi, sunt delectus.</p>
+            <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
+            <p class="text-gray-7"> {{ car.description }}</p>
           </div>
-          <h1 class="mt-auto text-xl">$39,555</h1>
+          <h1 class="mt-auto text-xl">{{ car.price }}</h1>
         </div>
       </div>
     </div>
 </template>
-<script setup></script>
+<script setup>
+const props = defineProps({
+  car: Object
+}) 
+</script>
