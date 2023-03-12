6de5bec9c45e0c02f47fe91c5c9ac211bf28927e
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-03-12 16:56:51 -0700

    Update [name]-[id].vue

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -18,7 +18,14 @@ const car = computed(()=>{
     return cars.find((c) => {
         return c.id === parseInt(route.params.id)
     })
-})
+});
+
+if(!car.value) {
+    throw createError({
+        statusCode: 404,
+        message: `Car with id of ${route.params.id} does not exist`
+    })
+}
 
 definePageMeta({
     layout: "custom"
