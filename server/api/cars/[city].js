516daee23d22e84638f205597d1938df3182a71e
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-03-13 11:45:19 -0700

    extrcting the path parameters

--- server/api/cars/[city].js
+++ server/api/cars/[city].js
@@ -1,3 +1,10 @@
+import cars from "@/data/cars.json"
+
 export default defineEventHandler((event) => {
-    return "Hello World"
+    const {city} = event.context.params;
+    let filteredCars = cars;
+    filteredCars = filteredCars.filter(car => {
+       return car.city.toLowerCase() === city.toLowerCase()
+    })
+    return filteredCars;
 })
