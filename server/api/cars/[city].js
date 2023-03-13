db8caeb59e450d3553fff9114a4c288adf27df7c
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-03-13 11:53:24 -0700

    extracting the Query parameters

--- server/api/cars/[city].js
+++ server/api/cars/[city].js
@@ -2,9 +2,15 @@ import cars from "@/data/cars.json"
 
 export default defineEventHandler((event) => {
     const {city} = event.context.params;
+    const {make, minPrice, maxPrice} = getQuery(event);
     let filteredCars = cars;
     filteredCars = filteredCars.filter(car => {
        return car.city.toLowerCase() === city.toLowerCase()
-    })
+    });
+    if(make) {
+    filteredCars = filteredCars.filter(car => {
+        return car.make.toLowerCase() === make.toLowerCase()
+        });
+    }
     return filteredCars;
 })
