3c2eef2ba33b73dde3afa274aa3402146aa5b85f
Nizami Suleymanov <68691336+Nizami9@users.noreply.github.com>
2023-03-12 17:27:11 -0700

    handle client side errors

--- components/CarSideBar.vue
+++ components/CarSideBar.vue
@@ -2,7 +2,7 @@
     <div class="shadow border w-1/2 mr-10 z-30 h-[190px]">
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Location</h3>
-      <h3 @click="updateModal('location')" class="text-blue-400 capitalize">Toronto</h3>
+      <h3 @click="updateModal('location')" class="text-blue-400 capitalize">{{ route.params.city }}</h3>
         <div v-if="modal.location" class="absolute border shadow left-96 p-5 top-1 -m-1 bg-white">
           <input type="text" class="border p-1 rounded" v-model="city">
           <button @click="onChangeLocation" class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
@@ -33,7 +33,13 @@ const updateModal = (key) => {
 const city = ref("");
 const route = useRoute();
 const onChangeLocation = () => {
-  if(!city.value) return
+  if(!city.value) return;
+  if(!isNaN(parseInt(city.value))) {
+    throw createError({
+      statusCode: 400,
+      message: "Invalid city format"
+    })
+  }
   updateModal('location')
   navigateTo(`/city/${city.value}/car/${route.params.make}`)
   city.value = "";
